ce2e8e6bd5cf76987cf51985e234f38f8aca4ce0
Caren-cheruto <cherutocaren43@gmail.com>
2022-09-08 18:40:36 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,13 @@
 #include <unistd.h>
 
 /**
-* main - print quote
-*
-* Description: prints Dora's quote
+* main - Event point
 *
 * Return: Always 1 (Success)
 */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
